f8cc89596f4711c41cc23c391e2f20836a34d244
Preetham Pemmasani <75422607+ppmpreetham@users.noreply.github.com>
2024-04-07 15:51:19 +0530

    Initial commit

---
+++ thiefbuster/src/routes/+page.svelte
@@ -0,0 +1,43 @@
+<nav class="navbar">
+    <ul class="navbar-list">
+        <li><img src="logo.png" alt="Logo"></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#usage">Usage</a></li>
+        <li><a href="#etc">Etc</a></li>
+    </ul>
+</nav>
+
+<h1>THIEF BUSTER</h1>
+<p>Your AI companion for catching thieves.</p>
+
+<style>
+    .navbar {
+        position: sticky;
+        top: 0;
+        background-color: transparent;
+        padding: 10px;
+    }
+
+    .navbar-list {
+        list-style-type: none;
+        margin: 0;
+        padding: 0;
+        overflow: hidden;
+    }
+
+    .navbar-list li {
+        float: left;
+    }
+
+    .navbar-list li a {
+        display: block;
+        color: #333;
+        text-align: center;
+        padding: 14px 16px;
+        text-decoration: none;
+    }
+
+    .navbar-list li a:hover {
+        text-decoration: underline;
+    }
+</style>
