5070149fb77283454133e6d90b60bf61b5b05ca4
Preetham Pemmasani <75422607+ppmpreetham@users.noreply.github.com>
2024-04-08 09:36:49 +0530

    Responsive Header

--- thiefbuster/src/routes/+page.svelte
+++ thiefbuster/src/routes/+page.svelte
@@ -1,22 +1,64 @@
-<nav class="navbar">
-    <ul class="navbar-list">
-        <li><img src="logo.png" alt="Logo"></li>
+<!-- before scrolled -->
+
+<nav class="navbar" class:scrolled={show}>
+  <ul class="navbar-list">
+    <li><img src="logo.png" alt="Logo"></li>
         <li><a href="#about">About</a></li>
         <li><a href="#usage">Usage</a></li>
         <li><a href="#etc">Etc</a></li>
+        <a style="--clr: #7808d0; float: right;" class="button" href="#demo">
+            <span class="button__icon-wrapper">
+              <svg width="10" class="button__icon-svg" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 15">
+                    <path fill="currentColor" d="M13.376 11.552l-.264-10.44-10.44-.24.024 2.28 6.96-.048L.2 12.56l1.488 1.488 9.432-9.432-.048 6.912 2.304.024z"></path>
+                </svg>
+                
+                <svg class="button__icon-svg  button__icon-svg--copy" xmlns="http://www.w3.org/2000/svg" width="10" fill="none" viewBox="0 0 14 15">
+                  <path fill="currentColor" d="M13.376 11.552l-.264-10.44-10.44-.24.024 2.28 6.96-.048L.2 12.56l1.488 1.488 9.432-9.432-.048 6.912 2.304.024z"></path>
+                </svg>
+              </span>
+              Try the Demo
+            </a>
     </ul>
 </nav>
 
+<!-- after scrolled -->
+<nav class:scrolled={show} style="margin-top: 10px;">
+  {#if show}
+  <nav class="navbar">
+    <ul class="navbar-list">
+        <li><img src="logo.png" alt="Logo"></li>
+        <a style="--clr: #7808d0; float: right;" class="button" href="#demo">
+            <span class="button__icon-wrapper">
+                <svg width="10" class="button__icon-svg" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 15">
+                    <path fill="currentColor" d="M13.376 11.552l-.264-10.44-10.44-.24.024 2.28 6.96-.048L.2 12.56l1.488 1.488 9.432-9.432-.048 6.912 2.304.024z"></path>
+                </svg>
+                
+                <svg class="button__icon-svg  button__icon-svg--copy" xmlns="http://www.w3.org/2000/svg" width="10" fill="none" viewBox="0 0 14 15">
+                     <path fill="currentColor" d="M13.376 11.552l-.264-10.44-10.44-.24.024 2.28 6.96-.048L.2 12.56l1.488 1.488 9.432-9.432-.048 6.912 2.304.024z"></path>
+                </svg>
+            </span>
+            Try the Demo
+        </a>
+    </ul>
+</nav>
+  {/if}
+</nav>
+
+<!-- Page elements -->
 <h1 class="title"><span class="pinyon-script">T </span>HIEF BUSTER</h1>
-<p class="subtitle">Your AI companion for catching thieves.</p>
+<p class="subtitle">Your AI companion for catching criminals.</p>
+<p></p>
 
+<!-- CSS -->
 <style>
-
     @font-face{
         font-family: HelveticaDisplay;
         src: url("../src/assets/fonts/HelveticaNowDisplay-Regular.otf")
     }
-    
+
+    *{
+        font-family: HelveticaDisplay;
+    }
     @font-face {
     font-family: Roslindale;
     src: url("../src/assets/fonts/RoslindaleVariableDisplay.ttf");
@@ -36,9 +78,28 @@
     font-style: normal;
     }
     
+    .helveticaDisplay{
+        font-family: helveticaDisplay;
+    }
+
+    .scrolled {transform: translate(0,calc(-100% - 1rem))}
+    
+    nav {
+      position: fixed;
+      transition: 0.5s ease      
+    }
+    
+    :global(body) {
+        margin: 0;
+        padding: 0;
+        height: 200vh;
+    }
+
     .subtitle{
+        font-family: helveticaDisplay;
         font-weight: 300;
-        font-size: 1rem;
+        font-size: 1.4rem;
+        margin-top: 0rem;
         margin: 0 auto;
         text-align: center;
         align-items: center;
@@ -47,6 +108,7 @@
     .title {
         font-family: roslindale;
         font-weight: lighter;
+        height: 0vh;
         font-size: 6rem;
         margin: 25vh auto;
         text-align: center;
@@ -74,7 +136,59 @@
     .navbar-list li {
         float: left;
     }
+    .button {
+      line-height: 1;
+      text-decoration: none;
+      display: inline-flex;
+      align-items: center;
+      gap: .75rem;
+      background-color: var(--clr);
+      color: #fff;
+      border-radius: 10rem;
+      font-weight: 600;
+      padding: .75rem 1.5rem;
+      padding-left: 20px;
+      white-space: nowrap;
+      overflow: hidden;
+      text-overflow: ellipsis;
+      transition: background-color .3s;
+    }
     
+    .button__icon-wrapper {
+      flex-shrink: 0;
+      width: 25px;
+      height: 25px;
+      position: relative;
+      color: var(--clr);
+      background-color: #fff;
+      border-radius: 50%;
+      display: grid;
+      place-items: center;
+      overflow: hidden;
+    }
+    
+    .button:hover {
+      background-color: #000;
+    }
+    
+    .button:hover .button__icon-wrapper {
+      color: #000;
+    }
+    
+    .button__icon-svg--copy {
+      position: absolute;
+      transform: translate(-150%, 150%);
+    }
+
+    .button:hover .button__icon-svg:first-child {
+      transition: transform .3s ease-in-out;
+      transform: translate(150%, -150%);
+    }
+
+    .button:hover .button__icon-svg--copy {
+      transition: transform .3s ease-in-out .1s;
+      transform: translate(0);
+    }
     .navbar-list li a {
         display: block;
         color: #333;
@@ -88,21 +202,22 @@
     }
 </style>
 
+<!-- Script -->
 <script>
-    import { onMount } from 'svelte';
-
-    onMount(() => {
-        const navbar = document.querySelector('.navbar');
-
-        window.addEventListener('scroll', () => {
-            const navbar = document.querySelector('.navbar');
-            if (navbar instanceof HTMLElement && window.scrollY > navbar.offsetHeight) {
-                navbar.classList.add('scrolled');
-            } else {
-                if (navbar) {
-                    navbar.classList.remove('scrolled');
-                }
-            }
-        });
-    });
+  import {onMount, onDestroy} from 'svelte'
+  const scrollNavBar = 60
+  let show = false
+  onMount(() => {
+      window.onscroll = () => {
+          if (window.scrollY > scrollNavBar) {
+              show = true
+          } else {
+              show = false
+          }
+      }
+  })
+  
+  onDestroy(() => {
+      window.onscroll = () => {}
+  })
 </script>
