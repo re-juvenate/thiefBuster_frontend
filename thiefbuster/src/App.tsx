8eef1245bd64591eb157ddb6475c779a222efe9b
Preetham Pemmasani <75422607+ppmpreetham@users.noreply.github.com>
2024-04-11 22:08:27 +0530

    Added pages

--- thiefbuster/src/App.tsx
+++ thiefbuster/src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css'
+import demo from './pages/demo.tsx'
 import Navbar from './componets/navbar'
 import PageElement from './componets/page_element'
 
 function App() {
 
   return (
+    
     <>
       <Navbar/>
       <PageElement/>
